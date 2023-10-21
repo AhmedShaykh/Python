23fa2f04fe8b40e378934d6379d07d5b81fcd48b
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-10-21 15:20:45 +0500

    Add Chapter 1

--- Chapter_1/main.py
+++ Chapter_1/main.py
@@ -1,1 +1,13 @@
 print("Hello Python 3.12");
+
+message = "Learning Generative AI!";
+
+print(message);
+
+print(type(message));
+
+print(id(message)); # Physical Address
+
+print(dir(message)); # Methods & Attributes
+
+print([i for i in dir(message) if "__" not in i]); # All Methods & Attributes
