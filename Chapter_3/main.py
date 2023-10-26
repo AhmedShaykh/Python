bfbc97b7174d74e8d6513628a316e0a50a2efcfc
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-10-26 20:08:42 +0500

    Add Chapter 3

---
+++ Chapter_3/main.py
@@ -0,0 +1,15 @@
+from typing import List;
+
+beros: List[str] = ["Bilal", "Mani", "Yaseen", "Saqlain", "Majid"];
+
+print(beros);
+
+print(beros[0]);
+
+print(beros[2]);
+
+print(beros[1].upper());
+
+message: str = f"{beros[1]} My Class Follow";
+
+print(message);
