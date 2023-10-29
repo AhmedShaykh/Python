138863de040afd429793412b76417911a72a4952
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-10-29 20:37:04 +0500

    Add Chapter 4

---
+++ Chapter_4/main.py
@@ -0,0 +1,29 @@
+from typing import List;
+
+records: List[str] = ["Revealed", "Spinnin", "Armada", "Monstercat", "STMPD"];
+
+for record in records: # Indentation
+    
+    print(f"{record} \n");
+
+print("Is Top EDM Records Labels");
+
+from typing import Tuple;
+
+data_base : List[Tuple[str,str]] = [("Ahmed","456"), ("Bilal","628")];
+
+for row in data_base:
+    
+    user, number = row;
+
+    print(user, number);
+
+names: List[str] = ["Ahmed", "Saleem", "Shaikh"];
+
+for name in names:
+
+    print(name);
+
+    break;
+
+print("Ahmed Shaykh");
