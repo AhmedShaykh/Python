ee2572c12fcbfe65f95be2ef265df7ff50352dc7
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-11-04 01:00:08 +0500

    Add Chapter 7

--- Chapter_7/maIn.py
+++ Chapter_7/maIn.py
@@ -1,9 +1,96 @@
-import sys;
+from typing import Dict, List;
 
-print("Line 1");
+name : str = input("What Is Your Name?: ");
 
-print("Line 2");
+print(type(name));
 
-print(sys.argv);
+print(f"My Name Is {name}!");
 
-print(type(sys.argv));
+flag : bool = True;
+
+number : int = 0;
+
+while flag:
+
+    print(f"No. {number}");
+
+    number += 1;
+
+    if number == 10:
+
+        break;
+
+data : List[Dict[str,str]] = [];
+
+flag : bool = True;
+
+while flag:
+
+    print("Write Quit Or Exit To Stop This Program");
+
+    name : str = input("Enter Your Name: ");
+
+    eduction: str = input("Enter Your Education: ");
+
+    if name in ["Exit","Quit","Close","Stop"] or eduction in ["exit","quit","close","stop"]:
+
+        flag = False
+
+        break;
+
+    data.append(
+                { "name":name,
+                  "education": eduction }
+                );
+
+print(data);
+
+for i in range(1, 11):
+
+    if i == 5:
+
+        print(f"Number {i}");
+
+        continue;
+    
+    print(i);
+
+if True:
+
+    pass; # Escape The Block Logic Apply Go To Next Logic
+
+print("Free Palestine");
+
+prompt: str = "Enter Value:";
+
+active = True;
+
+while active:
+
+    message = input(prompt);
+
+    if message == "Quit":
+
+        active = False;
+    
+    else:
+
+        print(message);
+
+unConfirmedUsers: list[str] = ["Bilal", "Yaseen", "Usman"];
+
+confirmedUsers: list[str] = [];
+
+while unConfirmedUsers:
+
+    currentUser = unConfirmedUsers.pop();
+
+    print(f"Verifying user: {currentUser.upper()}");
+
+    confirmedUsers.append(currentUser);
+
+print("\nThe following users have been confirmed:");
+
+for confirmedUser in confirmedUsers:
+
+    print(confirmedUser);
