acba900dc96e94ed328af1eecb574fff137beea8
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-11-10 22:11:50 +0500

    Add Callable Function

--- Chapter_10/main.py
+++ Chapter_10/main.py
@@ -1,89 +1,89 @@
-class Teacher:
+# class Teacher:
 
-    def __init__(self, teacherId : int, teacherName : str) -> None: # Method / Constructor (__Init__)
+#     def __init__(self, teacherId : int, teacherName : str) -> None: # Method / Constructor (__Init__)
 
-        self.name : str = teacherName; # Attribute
+#         self.name : str = teacherName; # Attribute
 
-        self.teacherId : int  = teacherId;
+#         self.teacherId : int  = teacherId;
 
-        self.organizationName: str = "PIAIC";
+#         self.organizationName: str = "PIAIC";
 
-    def quarter(self, quarter : int ) -> None: # Method
+#     def quarter(self, quarter : int ) -> None: # Method
 
-        print(f"This Is Quarter {quarter}");
+#         print(f"This Is Quarter {quarter}");
     
-    def teaching(self, subject : str ) -> None:
+#     def teaching(self, subject : str ) -> None:
 
-        print(f"{self.name} Is Teaching {subject}...!");
+#         print(f"{self.name} Is Teaching {subject}...!");
 
-obj1 : Teacher = Teacher(1, "Sir Zia Khan");
+# obj1 : Teacher = Teacher(1, "Sir Zia Khan");
 
-obj2 : Teacher = Teacher(2, "Sir Qasim");
+# obj2 : Teacher = Teacher(2, "Sir Qasim");
 
-print(f"Name: {obj1.name}, ID: {obj1.teacherId} & Organiztion: {obj1.organizationName} \n");
+# print(f"Name: {obj1.name}, ID: {obj1.teacherId} & Organiztion: {obj1.organizationName} \n");
 
-print(f"Name: {obj2.name}, ID: {obj2.teacherId} & Organiztion: {obj2.organizationName} \n");
+# print(f"Name: {obj2.name}, ID: {obj2.teacherId} & Organiztion: {obj2.organizationName} \n");
 
-obj1.teaching("BlockChain")
+# obj1.teaching("BlockChain")
 
-obj2.teaching("Generative AI")
+# obj2.teaching("Generative AI")
 
-print(dir(obj1));
+# print(dir(obj1));
 
-class Teacher:
+# class Teacher:
 
-    counter : int = 0; # Variable
+#     counter : int = 0; # Variable
 
-    classTiming : str = "8 PM";
+#     classTiming : str = "8 PM";
 
-    def __init__(self, teacherId : int, teacherName : str) -> None:
+#     def __init__(self, teacherId : int, teacherName : str) -> None:
 
-        self.name : str = teacherName;
+#         self.name : str = teacherName;
 
-        self.teacherId : int  = teacherId;
+#         self.teacherId : int  = teacherId;
 
-        self.organizationName: str = "PIAIC";
+#         self.organizationName: str = "PIAIC";
     
-        Teacher.counter += 1;
+#         Teacher.counter += 1;
 
-    def quarter(self, quarter : int ) -> None:
+#     def quarter(self, quarter : int ) -> None:
 
-        print(f"This Is Quarter {quarter}");
+#         print(f"This Is Quarter {quarter}");
     
-    def teaching(self, subject : str ) -> None:
+#     def teaching(self, subject : str ) -> None:
 
-        print(f"{self.name} Is Teaching {subject}...!");
+#         print(f"{self.name} Is Teaching {subject}...!");
 
-    def details(self) -> None:
+#     def details(self) -> None:
 
-        information : str = f"""
-        Teacher Name Is {self.name}
-        Class Timing is {Teacher.classTiming}
-        """;
+#         information : str = f"""
+#         Teacher Name Is {self.name}
+#         Class Timing is {Teacher.classTiming}
+#         """;
         
-        print(information);
+#         print(information);
 
-print(Teacher.counter);
+# print(Teacher.counter);
 
-obj1 : Teacher = Teacher(1, "Sir Zia Khan"); # Create New Object Then Increase The Value Of Counter
+# obj1 : Teacher = Teacher(1, "Sir Zia Khan"); # Create New Object Then Increase The Value Of Counter
 
-print(obj1.counter); # And Also Run The Object Increase The Value Of Counter
+# print(obj1.counter); # And Also Run The Object Increase The Value Of Counter
 
-print(obj1.counter);
+# print(obj1.counter);
 
-print(Teacher.counter);
+# print(Teacher.counter);
 
-obj2 : Teacher = Teacher(2, "Sir Qasim");
+# obj2 : Teacher = Teacher(2, "Sir Qasim");
 
-print(obj1.counter);
+# print(obj1.counter);
 
-print(obj2.counter);
+# print(obj2.counter);
 
-print(Teacher.counter);
+# print(Teacher.counter);
 
-obj1.details();
+# obj1.details();
 
-print(id(obj2));
+# print(id(obj2));
 
 # ================== Inheritance ================== #
 
@@ -448,3 +448,137 @@ obj2 : Ahmed = Ahmed();
 obj2.drinking("Tea");
 
 print(dir(obj2));
+
+def greet(): # Callable
+    
+    print("Martin Garrix");
+
+greet.__call__(); # Callable Method Is Built-In Method In Every Object
+
+print(dir(greet));
+
+class MainWindow:
+
+    def show(self):
+
+        print("Showing The App Main Window...");
+
+    def __call__(self):
+
+        self.show();
+
+window = MainWindow();
+
+window(); # Callable Is Built-In Method So I Not Give The Method Name But It Run
+
+class Human(object):
+
+    def eating(self, food : str) -> None:
+
+        print(f"Human Is Eating {food}");
+
+    def __call__(self) -> None: # Default Run
+
+        self.eating("Nihari!");
+
+obj : Human = Human();
+
+obj();
+
+obj.eating("Biryani");
+
+print(type(Human));
+
+print(dir(type)); # Call Method Is Built-In In Every Object
+
+class SampleClass:
+     
+     def method(self):
+         
+         print("You Called Method!");
+
+sampleInstance = SampleClass();
+
+print(dir(sampleInstance.method)); # Show All Object Method's
+
+class NonCallable:
+
+    def __call__(self):
+
+        raise TypeError("Not Really Callable");
+    
+instance : NonCallable = NonCallable();
+
+print(callable(instance));
+
+print(callable(NonCallable));
+
+class PowerFactory:
+
+    def __init__(self, exponent: int = 2):
+
+        self.exponent = exponent;
+
+    def __call__(self, base: int):
+
+        return base ** self.exponent;
+    
+a : PowerFactory = PowerFactory(); # Run __init__
+
+print(a.exponent);
+
+print(a(8));
+
+class CumulativeAverager:
+
+    def __init__(self):
+
+        self.data = [];
+
+    def __call__(self, newValue):
+
+        self.data.append(newValue);
+
+        print(self.data);
+
+        return sum(self.data) / len(self.data);
+
+streamAverage = CumulativeAverager();
+
+print(streamAverage.data);
+
+print(streamAverage(12)); # Data = [12] (12/1)
+
+print(streamAverage(13)); # Data = [12, 13] (12 + 13 / 2)
+
+print(streamAverage(11));
+
+print(streamAverage(10));
+
+class Factorial:
+
+    def __init__(self):
+
+        self.cache = {0: 1, 1: 1};
+
+    def __call__(self, number):
+
+        if number not in self.cache:
+
+            self.cache[number] = number * self(number - 1);
+        
+        return self.cache[number];
+
+factorialOf = Factorial();
+
+print(factorialOf.cache);
+
+factorialOf(4);
+
+factorialOf(5);
+
+factorialOf(4);
+
+factorialOf(10);
+
+print(factorialOf.cache);
